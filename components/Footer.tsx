ba95361e9fba5ee62b5dd61eecd887c1c46adef5
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-01-23 19:40:08 -0300

    adding rel='noopener'

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,21 +6,21 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       <footer className='header'>
-            <a href="https://www.lavarinimoreira.me" target="_blank">lavarinimoreira</a>
+            <a href="https://www.lavarinimoreira.me" target="_blank" rel='noopener'>lavarinimoreira</a>
             <div className={styles.footer__div}>
-                <a href="https://www.github.com/lavarinimoreira" target="_blank">
+                <a href="https://www.github.com/lavarinimoreira" target="_blank" rel='noopener'>
                     <AiFillGithub />
                 </a>
-                <a href="mailto:gabriel@lavarinimoreira.me" target="_blank">
+                <a href="mailto:gabriel@lavarinimoreira.me" target="_blank" rel='noopener'>
                     <AiOutlineMail />
                 </a>
-                <a href="https://www.linkedin.com/in/gabriel-lavarini-moreira-878706149/" target="_blank">
+                <a href="https://www.linkedin.com/in/gabriel-lavarini-moreira-878706149/" target="_blank" rel='noopener'>
                     <AiOutlineLinkedin />
                 </a>
-                <a href="https://twitter.com/lavarinimoreira" target="_blank">
+                <a href="https://twitter.com/lavarinimoreira" target="_blank" rel='noopener'>
                     <AiOutlineTwitter />
                 </a>
-                <a href="https://www.instagram.com/gabriel_lavarini/" target="_blank">
+                <a href="https://www.instagram.com/gabriel_lavarini/" target="_blank" rel='noopener'>
                     <AiFillInstagram />
                 </a>
             </div>
